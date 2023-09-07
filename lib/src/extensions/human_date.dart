6da519da36bfdf24496a738ff26efbddc58f6b5c
Mustafa Ibrahim <Mustafa7ibrahim@gmail.com>
2023-09-07 16:31:41 +0300

    improve dates

--- lib/src/extensions/human_date.dart
+++ lib/src/extensions/human_date.dart
@@ -1,23 +1,75 @@
 import 'package:intl/intl.dart';
 
-/// ToHumanDate extension on DateTime class
-/// to convert DateTime to human readable date
-extension ToHumanDate on DateTime {
-  /// toHumanDate method to convert DateTime to human readable date
-  String toHumanDate() {
-    final now = DateTime.now();
-    final difference = now.difference(this);
+enum DateDisplayType { full, short }
 
-    if (difference.inDays >= 7) {
-      return DateFormat('MMM d ' 'at' ' h:mm a').format(this);
-    } else if (difference.inDays >= 1) {
-      return '${difference.inDays} ${difference.inDays == 1 ? 'day' : 'days'} ago';
-    } else if (difference.inHours >= 1) {
-      return '${difference.inHours} ${difference.inHours == 1 ? 'hour' : 'hours'} ago';
-    } else if (difference.inMinutes >= 1) {
-      return '${difference.inMinutes} ${difference.inMinutes == 1 ? 'minute' : 'minutes'} ago';
+enum DateDisplayLanguage { en, ar }
+
+extension ToHumanDate on DateTime {
+  String toHumanDate({
+    DateDisplayType displayType = DateDisplayType.short,
+    DateDisplayLanguage language = DateDisplayLanguage.en,
+  }) {
+    final now = DateTime(
+      DateTime.now().year,
+      DateTime.now().month,
+      DateTime.now().day + 1,
+    );
+
+    final difference = now.difference(this);
+    final isFuture = isAfter(now);
+
+    if (isFuture) {
+      if (difference.inDays >= 7) {
+        return DateFormat(
+                displayType == DateDisplayType.full ? 'MMM d, y' : 'MMM d')
+            .format(this);
+      } else if (difference.inDays >= 1) {
+        if (language == DateDisplayLanguage.ar) {
+          return 'بعد ${difference.inDays} ${difference.inDays == 1 ? 'يوم' : 'أيام'}';
+        }
+        return '${difference.inDays} ${difference.inDays == 1 ? 'day' : 'days'} from now';
+      } else if (difference.inHours >= 1) {
+        if (language == DateDisplayLanguage.ar) {
+          return 'بعد ${difference.inHours} ${difference.inHours == 1 ? 'ساعة' : 'ساعات'}';
+        }
+        return '${difference.inHours} ${difference.inHours == 1 ? 'hour' : 'hours'} from now';
+      } else if (difference.inMinutes >= 1) {
+        if (language == DateDisplayLanguage.ar) {
+          return 'بعد ${difference.inMinutes} ${difference.inMinutes == 1 ? 'دقيقة' : 'دقائق'}';
+        }
+        return '${difference.inMinutes} ${difference.inMinutes == 1 ? 'minute' : 'minutes'} from now';
+      } else {
+        if (language == DateDisplayLanguage.ar) {
+          return 'الآن';
+        }
+        return 'Just now';
+      }
     } else {
-      return 'Just now';
+      if (difference.inDays >= 7) {
+        return DateFormat(
+                displayType == DateDisplayType.full ? 'MMM d, y' : 'MMM d')
+            .format(this);
+      } else if (difference.inDays >= 1) {
+        if (language == DateDisplayLanguage.ar) {
+          return 'منذ${difference.inDays} ${difference.inDays == 1 ? 'يوم' : 'أيام'}';
+        }
+        return '${difference.inDays} ${difference.inDays == 1 ? 'day' : 'days'} ago';
+      } else if (difference.inHours >= 1) {
+        if (language == DateDisplayLanguage.ar) {
+          return 'منذ ${difference.inHours} ${difference.inHours == 1 ? 'ساعة' : 'ساعات'}';
+        }
+        return '${difference.inHours} ${difference.inHours == 1 ? 'hour' : 'hours'} ago';
+      } else if (difference.inMinutes >= 1) {
+        if (language == DateDisplayLanguage.ar) {
+          return 'منذ ${difference.inMinutes} ${difference.inMinutes == 1 ? 'دقيقة' : 'دقائق'}';
+        }
+        return '${difference.inMinutes} ${difference.inMinutes == 1 ? 'minute' : 'minutes'} ago';
+      } else {
+        if (language == DateDisplayLanguage.ar) {
+          return 'الآن';
+        }
+        return 'Just now';
+      }
     }
   }
 }
