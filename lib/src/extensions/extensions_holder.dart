c1ec68ac38a568ad85dabd79759ba6e6eea3a2cc
Mustafa Ibrahim <Mustafa7ibrahim@gmail.com>
2024-05-09 17:57:11 +0300

    feat: Add time extensions for TimeOfDay and time comparison functions

--- lib/src/extensions/extensions_holder.dart
+++ lib/src/extensions/extensions_holder.dart
@@ -2,3 +2,5 @@ export 'empty_padding.dart';
 export 'format_as_his.dart';
 export 'format_as_ymd.dart';
 export 'human_date.dart';
+export 'time_extensions.dart';
+export 'time_of_day_extensions.dart';
