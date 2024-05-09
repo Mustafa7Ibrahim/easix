cceabd2e81c1f9b8bb3f9ca96a179bc50b7bb36a
Mustafa Ibrahim <Mustafa7ibrahim@gmail.com>
2024-05-09 18:01:49 +0300

    feat: Improve string initials extension
    The code changes add a new method to the `StringInitials` extension that allows getting the initials of a string. It also includes a new method to get the initial of a string. This enhancement improves the functionality of the extension and provides more flexibility for obtaining initials from strings.

--- lib/src/extensions/string_initials.dart
+++ lib/src/extensions/string_initials.dart
@@ -1,9 +1,33 @@
 extension StringInitials on String {
+  /// Get initials from a string
+  /// example:
+  /// ```dart
+  /// 'John Doe'.initials;
+  /// ```
+  /// output:
+  /// ```dart
+  /// JD
+  /// ```
+  ///
   String get initials {
-    List<String> nameParts = split(' ');
+    List<String> nameParts = trim().split(' ');
     if (nameParts.length < 2) {
       return nameParts.first.substring(0, 1);
     }
-    return nameParts.first[0] + nameParts.last[0];
+    return nameParts.first.trim()[0] + nameParts.last.trim()[0];
+  }
+
+  /// Get initial from a string
+  /// example:
+  /// ```dart
+  /// 'John Doe'.initial;
+  /// ```
+  /// output:
+  /// ```dart
+  /// J
+  /// ```
+  ///
+  String get initial {
+    return trim().substring(0, 1);
   }
 }
