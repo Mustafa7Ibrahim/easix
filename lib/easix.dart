8308ebeda8b97f1589f532a2131552a410fd3c6a
Mustafa Ibrahim <Mustafa7ibrahim@gmail.com>
2024-05-09 17:57:03 +0300

    feat: Add string initials extension

--- lib/easix.dart
+++ lib/easix.dart
@@ -2,6 +2,7 @@ library easix;
 
 export 'src/error/error_holder.dart';
 export 'src/extensions/extensions_holder.dart';
+export 'src/extensions/string_initials.dart';
 export 'src/sort_list_by_date/sort_date_list.dart';
 export 'src/sort_list_by_date/sort_list_by_date.dart';
 export 'src/validate/validate.dart';
