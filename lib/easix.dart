053feb0e55b0b2375662cbb82c5134c42e8faf09
Mustafa Ibrahim <Mustafa7ibrahim@gmail.com>
2024-05-09 18:35:27 +0300

    feat: Add BuildContextExtensions and getTypeList exports
    This code change adds two new exports to the `easix.dart` file: `BuildContextExtensions` and `getTypeList`. The `BuildContextExtensions` extension provides convenient access to the theme, textTheme, and colorScheme properties of the BuildContext, improving code readability. The `getTypeList` function converts dynamic data into a list of objects of a specified type, reducing the need for manual conversion. These additions enhance the functionality and usability of the library.

--- lib/easix.dart
+++ lib/easix.dart
@@ -6,3 +6,5 @@ export 'src/extensions/string_initials.dart';
 export 'src/sort_list_by_date/sort_date_list.dart';
 export 'src/sort_list_by_date/sort_list_by_date.dart';
 export 'src/validate/validate.dart';
+export 'src/extensions/build_context_extensions.dart';
+export 'src/sort_list_by_date/get_type_list.dart';
